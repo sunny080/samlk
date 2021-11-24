cd1763e694cad399ace5ee1a4e289803533cae75
Sunny <77973332+sunny080@users.noreply.github.com>
2021-11-24 03:47:36 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const dooli = document.querySelector(".moll");
+const doli = document.querySelector(".lota");
+const dolia = document.querySelector("#dol");
+const doliae = document.querySelector("#dola");
+const doliaea = document.querySelector("#dolaa");
+const jhon = document.querySelector(".jhon");
+
+const jhos = document.querySelector(".kolo");
+
+const jrf = document.querySelector("ul");
+const jrfa = document.querySelector(".lio");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const tl = new TimelineMax();
+
+
+tl.fromTo(jhon, 1, {marginTop: '-100vh' }, { marginTop: '0vh' ,ease:Power2.easeIn})
+
+tl.fromTo(doli, 1, {marginLeft: '-100%' }, { marginLeft: '10%' ,ease:Power2.easeIn} ,"-=.2")
+
+tl.fromTo(dolia, 1, {marginLeft: '-86%' }, { marginLeft: '10%' ,ease:Power2.easeIn},"-=.8")
+
+tl.fromTo(doliae, 1, {marginLeft: '-86%' }, { marginLeft: '10%' ,ease:Power2.easeIn},"-=.8")
+
+tl.fromTo(doliaea, 1, {marginLeft: '-100%' }, { marginLeft: '10%' ,ease:Power2.easeIn},"-=.8")
+
+
+tl.fromTo(jhos, 1, {marginLeft: '-120%' }, { marginLeft: '0%' ,ease:Power2.easeInOut})
+
+tl.fromTo(jrf, 1, {marginTop: '-100px' }, { marginTop: '16px' ,ease:Power2.easeInOut},"-=.8")
+tl.fromTo(jrfa, 1, {marginTop: '-100%' }, { marginTop: '0%' ,ease:Power2.easeInOut},"-=1.3")
+
+
+
+
+
+
+
+
+
+
+
